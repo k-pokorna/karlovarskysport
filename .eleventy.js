fb52f0db5abfdf4f82597a4c05908a10baffd46c
Karolína Pokorná <k-pokorna@seznam.cz>
2023-05-20 21:06:04 +0200

    Vystavení projektu na GitHub a následně na Netlify

---
+++ .eleventy.js
@@ -0,0 +1,23 @@
+module.exports = function(eleventyConfig) {
+
+	// Výchozí výstupní složka: _site
+
+	// Zkopírovat images/ do _site/images
+	eleventyConfig.addPassthroughCopy("images");
+  // Zkopírovat downloads/ do _site/images
+  eleventyConfig.addPassthroughCopy("downloads");
+
+	// Zkopírovat css/ to _site/css/
+	eleventyConfig.addPassthroughCopy("css");
+
+  return {
+    // možné formáty šablon
+    templateFormats: ["njk", "html", "md", "liquid"],
+
+    // jako šablonovací jazyk zvolíme Nunjucks
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  }
+
+};
